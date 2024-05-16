bb7898bef78ac8d89d861fe6ccd503650d55c39e
Shawn-Mike <91728104+Shawn-Mike@users.noreply.github.com>
2024-05-15 23:18:25 -0400

    Create socketclient.py

---
+++ socketclient.py
@@ -0,0 +1,7 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 4242)
+s.send(b'Say something: ') # b tag added in python3 to indicate bytes not strings
+data = s.recv(1024)          
+s.close()
+print('Received', data)
